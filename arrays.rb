825349e45c4bba83bb24413327a7b281065b42eb
Brian Hayes <Brian.hayes1200@gmail.com>
2022-08-29 15:11:54 -0600

    add array methods

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,16 @@
 family_names = ["Brian", "John", "Carolyn", "Brad", "Margie"]
-age = [19, 18, 22, 55, 54]
+# Adding cat names
+family_names.push("Leo", "Casper")
+#list family names in reverse order
+family_names.pop
+puts family_names
+age = [54, 55, 22, 19, 18]
+# Adding cat ages
+age.unshift(5, 2)
 random_floats = [1.1, 2.2, 3.3, 4.4]
+#remove 1.1 from the array and return newly shifted array
+random_floats.shift
+puts random_floats
 random_facts = [1 is 1 = true, "I have cats" = true, "I love bananas" = false, "2 equals 3" = false]
+#add elements to the end of the array
+random_facts.push("I think I understand" = true)
