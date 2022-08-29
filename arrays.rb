ea076692a26b853793ea14f313c8735f040ffccd
Brian Hayes <Brian.hayes1200@gmail.com>
2022-08-29 15:27:57 -0600

    fixed bugs

--- arrays.rb
+++ arrays.rb
@@ -2,18 +2,20 @@ family_names = ["Brian", "John", "Carolyn", "Brad", "Margie"]
 # Adding cat names
 family_names.push("Leo", "Casper")
 #list family names in reverse order
-family_names.pop
 puts family_names
-age = [54, 55, 22, 19, 18]
+
+puts 'ages'
+age = [19, 18, 22, 55, 54]
 # Adding cat ages
 age.unshift(5, 2)
+puts age
 random_floats = [1.1, 2.2, 3.3, 4.4]
 #remove 1.1 from the array and return newly shifted array
 random_floats.shift
 puts random_floats
-a = 1 = 1
-b = 2 + 2 =4
-c = 3 = 4
+a = 1 == 1
+b = 2 + 2 == 4
+c = 3 == 4
 d = 'I love bananas'
 random_facts = [a = true, b = true, c = false, d = false]
 #add elements to the end of the array
