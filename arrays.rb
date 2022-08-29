f13ff377ac68dd18a63646dd0edaa03cd37ce191
Brian Hayes <Brian.hayes1200@gmail.com>
2022-08-29 15:40:49 -0600

    add new method

--- arrays.rb
+++ arrays.rb
@@ -3,7 +3,8 @@ family_names = ["Brian", "John", "Carolyn", "Brad", "Margie"]
 family_names.push("Leo", "Casper")
 #list family names in reverse order
 puts family_names
-
+puts 'My mom is'
+family_names.pop
 puts 'ages'
 age = [19, 18, 22, 55, 54]
 # Adding cat ages
@@ -21,3 +22,5 @@ random_facts = [a = true, b = true, c = false, d = false]
 #add elements to the end of the array
 e = "I think I understand"
 random_facts.push(e = true)
+#this will shuffle the random floats variable
+random_floats.shuffle
