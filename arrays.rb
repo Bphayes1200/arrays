699c6e4c0309508d762c4ec84b6c940fc0ee485b
Brian Hayes <Brian.hayes1200@gmail.com>
2022-08-29 13:53:11 -0600

    add variables and arrays

---
+++ arrays.rb
@@ -0,0 +1,4 @@
+family_names = ["Brian", "John", "Carolyn", "Brad", "Margie"]
+age = [19, 18, 22, 55, 54]
+random_floats = [1.1, 2.2, 3.3, 4.4]
+random_facts = [1 is 1 = true, "I have cats" = true, "I love bananas" = false, "2 equals 3" = false]
